0b376e9e37facb36e7c6546cf7d027bc7e4ae3c8
Vladislav <weyzemofficial@gmail.com>
2023-01-23 19:47:52 +0300

    update

--- md5_json.py
+++ md5_json.py
@@ -1,6 +1,5 @@
 import hashlib
 import os
-from datetime import datetime
 import json
 
 
@@ -17,19 +16,18 @@ def check_sum():
 
     for rootdir, dirs, files in os.walk(path):
         for file in files:       
-            if '.' in file:
-                # print (os.path.join(rootdir, file))
-                
-                with open(os.path.join(rootdir, file), 'rb') as get_file:
-                    byte_size = get_file.read()
-                                
-                    get_file.close()
+            # print (os.path.join(rootdir, file))
+            
+            with open(os.path.join(rootdir, file), 'rb') as get_file:
+                byte_size = get_file.read()
+                            
+                get_file.close()
 
-                    md5_list['files'].append({
-                        "size":len(byte_size),
-                        "name":str(os.path.join(rootdir, file)).replace('\\', '/'),
-                        "md5":str(hashlib.md5(byte_size).hexdigest())
-                    })
+                md5_list['files'].append({
+                    "size":len(byte_size),
+                    "name":str(os.path.join(rootdir, file)).replace('\\', '/'),
+                    "md5":str(hashlib.md5(byte_size).hexdigest())
+                })
                             
         with open(file_json+'.json', 'w') as json_file:
             json_file.write(json.dumps(md5_list))
